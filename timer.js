3a67c1112645d03f6cba2c22f47b0a3dd8faa222
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2021-08-16 02:57:37 +0300

    timer

--- timer.js
+++ timer.js
@@ -2,6 +2,7 @@ class CountdownTimer {
     constructor({selector, targetDate}){
       this.selector = selector;
       this.targetDate = targetDate;
+      this.intervalId = null;
     }
   
     getRefs () {
@@ -11,29 +12,39 @@ class CountdownTimer {
       const timerMins = timerContainer.querySelector('[data-value="mins"]');
       const timerSecs = timerContainer.querySelector('[data-value="secs"]');
       
-      return {timerDays, timerHours, timerMins, timerSecs};
+      return {timerContainer, timerDays, timerHours, timerMins, timerSecs};
     }
   
-    updateTimer ({timerDays, timerHours, timerMins, timerSecs}) {
+    updateTimer ({timerContainer, timerDays, timerHours, timerMins, timerSecs}) {
       const time = this.targetDate - Date.now();
-      timerDays.textContent = Math.floor(time / (1000 * 60 * 60 * 24));
-      timerHours.textContent = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-       timerMins.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-        timerSecs.textContent = Math.floor((time % (1000 * 60)) / 1000);
+      if (time < 0) {
+          this.stop(timerContainer);
+          return
+      }
+      timerDays.textContent = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+      timerHours.textContent = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
+      timerMins.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+      timerSecs.textContent = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, '0');
       
     }
 
     start () {
-        setInterval(() => {
+        this.intervalId = setInterval(() => {
             this.updateTimer(this.getRefs())
         }, 1000);
+    }
+
+    stop (timerContainer) {
+        clearInterval(this.intervalId);
+        timerContainer.innerHTML = "<h1>You're out of time!</h1>";
+        
     }
   }
   
        
   const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Sep 23, 2021'),
+    targetDate: new Date('Aug 16, 2021 02:56'),
   });
   
   timer.start();
